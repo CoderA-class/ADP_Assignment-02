d468092d13eb2be29f3d75966b2469d122da458d
David Daniel Sepkitt <240046935@mycput.ac.za>
2026-08-16 16:38:36 +0000

    Add React customer frontend

---
+++ src/test/java/za/ac/cput/adp_assignment02/CustomerControllerIntegrationTest.java
@@ -0,0 +1,13 @@
+package za.ac.cput.adp_assignment02;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class CustomerControllerIntegrationTest {
+
+    @Test
+    void appHasCustomerApiReady() {
+        assertTrue(true);
+    }
+}
