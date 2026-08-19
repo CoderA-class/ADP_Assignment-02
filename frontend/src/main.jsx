fa385c23e1ae7e61c80fce93b450f4ee2776ef4c
CoderA-class <attie90@outlook.com>
2026-08-19 17:48:27 +0200

    Added frontend for orderitem.jsx and OrderItemservice.jsx , updated main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles.css';
 import CustomerApp from './components/CustomerApp';
+import OrderItem from './components/orderitem.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <CustomerApp />
-  </React.StrictMode>
+    <React.StrictMode>
+        <CustomerApp />
+        <OrderItem />
+    </React.StrictMode>
 );
